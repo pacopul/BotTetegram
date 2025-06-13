6f7e5fef75ed2f17411db179303f33682441392b
pacopul <profesor.pacopul@iesazarquiel.es>
2025-06-13 11:45:58 +0200

    uno

---
+++ main.py
@@ -0,0 +1,32 @@
+import telebot
+import logging
+
+# Configuración de logs 
+# 💡 Emojis para logs ctrl cmd space 💡
+logging.basicConfig(
+    filename="telegram.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+# Token dekl bot de Telegram
+BOT_TOKEN = "7820202353:AAE2kTD8kIK_zIpZYcPiekSeCHEzQhTuRPU"
+bot = telebot.TeleBot(BOT_TOKEN)
+
+# Configuración de id de Telegram
+TELEGRAM_CHAT_ID = "272783109"
+
+def enviar_notificacion_telegram(mensaje):
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, mensaje)
+        logging.info("✅ Notificación enviada por Telegram.")
+    except telebot.apihelper.ApiException as e:
+        logging.error(f"⚠️ Error al enviar mensaje: {e}")
+        if "chat not found" in str(e):
+            logging.error("❌ Verifica que el ID del chat sea correcto.")
+        elif "Forbidden" in str(e):
+            logging.error("❌ El bot no tiene permisos para enviar mensajes.")
+    except Exception as e:
+        logging.error(f"❌ Error inesperado: {e}")
+   
+enviar_notificacion_telegram("Hola desde el bot")
